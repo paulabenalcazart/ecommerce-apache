812d4c7b2555800cea61eb71b6a496c3c8fa1298
Paula Benalcázar <paulabenalcazar7lp@gmail.com>
2025-12-12 20:29:33 -0500

    Creation of files for spark and schemas.py

---
+++ spark/schemas.py
@@ -0,0 +1,23 @@
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    DoubleType,
+    TimestampType
+)
+
+# Schema principal de eventos de e-commerce
+event_schema = StructType([
+    StructField("user_id", StringType(), True),
+    StructField("product_id", StringType(), True),
+    StructField("category", StringType(), True),
+    StructField("event_type", StringType(), True),  # view | add_to_cart | purchase
+    StructField("price", DoubleType(), True),
+    StructField("timestamp", TimestampType(), True),
+
+    # Informacion de la tienda (opcional)
+    StructField("store_id", StringType(), True),
+    StructField("store_type", StringType(), True),
+    StructField("city", StringType(), True),
+    StructField("province", StringType(), True)
+])
